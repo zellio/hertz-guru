5a372c2d9f27a42e10d45294373f7d7ac8f31f6e
Zachary Elliott <zach@nyu.edu>
2014-03-27 21:50:34 -0400

    First pass complete

--- web.rb
+++ web.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
 
 get '/' do
-  erb :"index.html"
+  redirect to("/440"), 302
+end
+
+get '/:freq' do
+
+  redirect to("/sin/#{params[:freq]}"), 302
+end
+
+get '/:type/:freq' do
+  wave_types = [:sin, :square, :sawtooth, :triangle]
+  type = params[:type].to_sym
+
+  redirect to ("/sin/#{params[:freq]}"), 302 unless wave_types.include?(type)
+
+
+  erb :"index.html", locals: { wave_type: wave_types.index(type), hz_val: params[:freq] }
+
 end
